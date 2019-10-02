762e93fc295f784f73682a379fba11988a2cced7
SylvainLangler <langlersylvain@gmail.com>
2019-10-02 18:01:03 +0200

    test git credentials

--- JS/tp1.js
+++ JS/tp1.js
@@ -5,7 +5,7 @@ let tabMails = ['leo@orange.fr','lina@hotmail.fr','bernard@orange.fr','kirl@gmai
 let stringMails = tabMails.toString();
 
 var reg = new RegExp("^[a-z0-9._-]*@[a-z]*.[a-z]{2,3}","gi");
-
+//
 tabMatchedMail = stringMails.match(reg);
 
 console.log(tabMatchedMail);
